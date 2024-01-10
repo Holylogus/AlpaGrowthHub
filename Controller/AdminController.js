2143af3963d37b54ba76f2a680c41a76065b212b
Holylogus <hamar.patrik94@gmail.com>
2024-01-10 20:12:03 +0100

    teszt

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -4,7 +4,7 @@ import Table from "../View/Admin/Table.js";
 export default class AdminController {
     constructor(){
         this.adminModel = new AdminModel();
-        this.adminModel.getData("books.json", this.megjelenit);
+        this.adminModel.getData("http://127.0.0.1:8000/api/books", this.megjelenit);
         $(window).on("deleteRow", (e) => {
             console.log(e.detail)
             this.adminModel.deleteData("books.json", e.detail)
